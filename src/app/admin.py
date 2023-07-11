9b17e025a7bfa5842ae4c8195461d8bb12bc3751
ARUN <137037092+arun-tmachine@users.noreply.github.com>
2023-07-11 21:29:19 +0530

    Delete src directory
    del

--- src/app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import BMIRecord
-
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'age' , 'height' , 'weight' , 'mobile_number' , 'bmi' , 'bmi_category')
-
-admin.site.register(BMIRecord, UserProfileAdmin)
--- src/app/serializers.py
+++
@@ -1,8 +0,0 @@
-from .models import BMIRecord
-from rest_framework import serializers
-
-
-class BMIRecordSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = BMIRecord
-        fields = '__all__'
